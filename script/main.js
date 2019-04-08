eb90524a94344aba9ef2c4f24f80d18d0de42078
EvanBell <876895046@qq.com>
2019-04-08 21:28:47 +0800

    添加滚动到对应位置的元素显示，且导航栏响应高亮

--- script/main.js
+++ script/main.js
@@ -1,14 +1,24 @@
+// 添加offset类
+const specialTags = document.querySelectorAll('[data-x]');
+specialTags.forEach((item, index) => {
+    specialTags[index].classList.add('offset')
+})
+findClosest()
+// 鼠标滚动事件
 window.onscroll = function () {
+    // 页面滚动导航栏添加黏贴效果
     let scrollY = window.scrollY;
     if (scrollY > 0) {
         topNavBar.classList.add('sticky');
     } else {
         topNavBar.classList.remove('sticky');
     }
+
+    findClosest()
 };
 
+// 鼠标移入移出导航栏导航添加下划线
 let liTags = document.getElementsByClassName('subMenuTigger');
-
 for (let i=0; i < liTags.length; i++) {
     liTags[i].onmouseenter = function(event) {
         event.currentTarget.classList.add('active');
@@ -18,16 +28,43 @@ for (let i=0; i < liTags.length; i++) {
     }
 };
 
+function findClosest () {
+    // 滚动到对应位置的元素显示，且导航栏响应高亮 
+    let minIndex = 0;
+    const specialTags = document.querySelectorAll('[data-x]');
+    const seeHeight = document.documentElement.clientHeight;
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    
+    specialTags.forEach((item, index) => {
+        const current = Math.abs(item.offsetTop - window.scrollY);
+        const min = Math.abs(specialTags[minIndex].offsetTop - window.scrollY);
+        specialTags[index].classList.remove('active')
+        if(specialTags[index].offsetTop < seeHeight + scrollTop - 500) {
+            minIndex = index;
+        }
+    })
+    const currentSection = specialTags[minIndex]
+    currentSection.classList.remove('offset')
+    // currentSection.classList.add('animate')
+    currentSection.classList.add('active')
+    const aTag = document.querySelector(`a[href="#${currentSection.id}"]`);
+    const liTag = aTag.parentNode;
+    for(let i = 0; i< liTag.parentNode.children.length; i ++){
+        liTag.parentNode.children[i].classList.remove('highlight');
+    }
+    liTag.classList.add('highlight')
+}
+
+
+
+// 点击导航平滑滚动到对应位置
+let aTags = document.querySelectorAll('.menu > li > a');
 function animate(time) {
     requestAnimationFrame(animate);
     TWEEN.update(time);
 }
 requestAnimationFrame(animate);
-
-let aTags = document.querySelectorAll('.menu > li > a');
-
 for (let i=0; i < aTags.length; i++) {
-    
     aTags[i].onclick = function(event) {
         event.preventDefault();
         const top = document.querySelector(event.currentTarget.getAttribute('href')).offsetTop;
@@ -44,25 +81,7 @@ for (let i=0; i < aTags.length; i++) {
                     window.scrollTo(0, coords.x)
                 })
                 .start();
-
-
-        /***
-         *  @手写运动框架
-            const n = 25;
-            const duration = 500 / n;
-            const currentTop = window.scrollY;
-            const targetTop = top - 80;
-            const distance = (targetTop - currentTop) / n;
-            let i = 0;
-            const timer = setInterval(() => {
-                if (i === n) {
-                    window.clearInterval(timer)
-                    return;
-                }
-                i = i+1;
-                window.scrollTo(0, currentTop + distance * i)
-            }, duration)
-         */
-        
     }
 };
+
+
